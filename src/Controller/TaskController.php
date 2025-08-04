4f2bf26e2120550655f12e9f51d3d29d264f5b60
Ilan Attias <ilan.attias@free.fr>
2025-08-05 00:43:51 +0200

    first route

---
+++ src/Controller/TaskController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TaskController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function home(): RedirectResponse
+    {
+        return $this->redirectToRoute('app_task');
+    }
+
+    #[Route('/task', name: 'app_task')]
+    public function index(): Response
+    {
+        return $this->render('task/index.html.twig', [
+            'controller_name' => 'TaskController',
+        ]);
+    }
+}
